9c48a0a91abeebd1a7e2fb662319bb24a2a4e524
bebo <ibrahimimran96@gmail.com>
2022-10-18 21:28:07 -0400

    Added routes, createNewNote function, and cleaned up code

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { createNewNote } = require("../../lib/notes");
+const notes  = require("../../db/db.json");
+
+router.get("/notes", (req, res) => {
+  let results = notes;
+  console.log(results);
+  res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length * 1996;
+  const note = createNewNote(req.body, notes);
+  res.json(note);  
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({
+  extended: true
+}));
+// parse incoming JSON data
+app.use(express.json());
+// Make everything within public directory readily available instead of keeping behind server endpoint
+app.use(express.static('public'));
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
